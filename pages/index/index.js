c80dbf0bead527fec9091864413fe37351d10fa9
fuyan <fuyan@jze100.com>
2019-02-18 18:55:53 +0800

    备忘录页面init

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,22 @@
 //index.js
 //获取应用实例
+
+const util = require("../../utils/util.js")
 const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    showAll: true,
+    lists: [],
+    newLi: {
+      id: "",
+      content: "",
+      begin: util.formatDate2(),
+    }
+
   },
   //事件处理函数
   bindViewTap: function() {
@@ -15,13 +24,13 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
+  onLoad: function() {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -34,6 +43,7 @@ Page({
       // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
         success: res => {
+          console.log(res)
           app.globalData.userInfo = res.userInfo
           this.setData({
             userInfo: res.userInfo,
@@ -44,11 +54,127 @@ Page({
     }
   },
   getUserInfo: function(e) {
-    console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
+    })
+  },
+  // 表单清空
+  formReset() {
+    this.setData({
+      'newLi.content': ''
+    })
+  },
+  // 表单提交 A
+  formSubmit() {
+    let newLists = this.data.lists,
+      i = 0,
+      newTodo = this.data.newLi;
+    if (newLists.length > 0) {
+      i = Number(util.sortBy(newLists, 'id', true)[0].id) + 1;
+    }
+    newTodo.id = i
+    if (newTodo.content != '') {
+      newLists.push(newTodo)
+      this.setData({
+        lists: newLists,
+        newLi: {
+          id: "",
+          content: "",
+          begin: util.formatDate2(),
+        }
+      })
+    }
+    this.remind();
+  },
+  //提醒功能
+  getRemindArr() {
+    console.log(1111)
+    let thisLists = this.data.lists,
+      closeT = 0,
+      notDoneLists = [];
+    let date = new Date(),
+      now = [date.getFullYear(), date.getMonth() + 1, date.getDate()]
+    thisLists.map(function(l) {
+      if (l.needRemind) {
+        notDoneLists.push(l)
+      }
+    })
+    if (notDoneLists.length > 0) {
+      let newLists = util.sortBy(notDoneLists, 'begin'),
+        firstT = (newLists[0].begin).split(':'),
+        id = newLists[0].id,
+        cnt = newLists[0].content;
+      closeT = ((firstT[0] - now[0]) * 60 + (firstT[1] - now[1]) - 1) * 60;
+      closeT = closeT >= 0 ? closeT : 0;
+      return {
+        closeT,
+        id,
+        cnt
+      };
+    } else {
+      return false;
+    }
+  },
+  //提醒功能提示框，判断是否已完成
+  remind() {
+    let result = this.getRemindArr(),
+      t = result.closeT,
+      id = result.id,
+      that = this,
+      cnt = result.cnt;
+
+    function alarm() {
+      let newLists = that.data.lists;
+      wx.showModal({
+        title: '已经完成了吗？',
+        content: cnt,
+        cancelText: '否',
+        confirmText: '是',
+        success: function(res) {
+          if (res.confirm) {
+            newLists.map(function(l, index) {
+              if (l.id == id) {
+                newLists[index].done = true;
+                newLists[index].needRemind = false;
+              }
+            })
+            that.setData({
+              lists: newLists
+            })
+          } else {
+            newLists.map(function(l, index) {
+              if (l.id == id) {
+                newLists[index].needRemind = false;
+              }
+            })
+            that.setData({
+              lists: newLists
+            })
+          }
+        }
+      })
+
+    }
+    if (result) {
+      setTimeout(alarm, Math.floor(t * 1000), function() {
+        that.remind();
+      })
+    }
+
+  },
+  // 输入框chang事件
+  iptChange(e) {
+    this.setData({
+      'newLi.content': e.detail.value,
+      'newLi.begin': util.formatDate2()
+    })
+  },
+  // picker 组件change事件
+  bindDateChange(e) {
+    this.setData({
+      "newLi.begin": e.detail.value
     })
   }
 })
--- utils/util.js
+++ utils/util.js
@@ -13,7 +13,48 @@ const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
+const sortBy = (arr, prop, desc) => {
+  let props = [],
+    ret = [],
+    i = 0,
+    len = arr.length;
+  if (typeof prop == 'string') {
+    for (; i < len; i++) {
+      let oI = arr[i];
+      (props[i] = new String(oI && oI[prop] || ''))._obj = oI;
+    }
+  }
+  if (typeof prop == 'Function') {
+    for (; i < len; i++) {
+      var oI = arr[i];
+      (props[i] = new String(oI && prop(oI) || ''))._obj = oI;
+    }
+  } else {
+    throw '参数类型错误';
+  }
+  props.sort();
+  for (i = 0; i < len; i++) {
+    ret[i] = props[i]._obj;
+  }
+  if (desc) ret.reverse();
+  return ret;
+}
+const formatDate2 =  ()=> {
+  var date = new Date();
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+  var nowYear = (new Date()).getFullYear();
+  var nowMonth = (new Date()).getMonth() + 1;
+
+
+  return year + '-' + month + '-' + day;
+
+  // return [ month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
 
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  sortBy: sortBy,
+  formatDate2: formatDate2,
 }
